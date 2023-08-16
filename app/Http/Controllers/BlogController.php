dcd51e21a116447527fe49208a42fcd40768bbcf
Camille <camille.mathien@gmail.com>
2023-08-16 18:07:09 +0200

    blog list and details views

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,21 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
-use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Contracts\Pagination\Paginator;
 
 class BlogController extends Controller
 {
-    public function index(): Paginator {
-        return Post::paginate(25);
+    public function index(): View
+    {
+        return view('blog.index', [
+            'posts' => Post::paginate(1)
+        ]);
     }
 
-    public function show(string $slug, string $id): RedirectResponse | Post {
+    public function show(string $slug, string $id): RedirectResponse | View
+    {
         $post = Post::findOrFail($id);
         if ($post->slug !== $slug) {
             return to_route('blog.show', ['slug' => $post->slug, 'id' => $post->id]);
         }
-        return $post;
+        return view('blog.show', [
+            'post' => $post
+        ]);
     }
 }
